62306dc350587fbbe2e3663599660f45d53a2c4d
Christian Watts <civrev@gmail.com>
2018-07-30 18:29:54 -0700

    text loading done

---
+++ PersonalSite/PersonalSite/Pages/Bio.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalSite.Pages
+{
+    public class BioModel : PageModel
+    {
+        public string bioParagraph { get; set; }
+
+        public void OnGet()
+        {
+            try
+            {
+                bioParagraph = System.IO.File.ReadAllText("wwwroot/txt/bio/bio.txt");
+            }
+            catch (Exception e)
+            {
+                bioParagraph = "Couldn't load text...";
+            }
+        }
+    }
+}
--- PersonalSite/PersonalSite/wwwroot/js/site.js
+++ PersonalSite/PersonalSite/wwwroot/js/site.js
@@ -1,1 +1,23 @@
 ﻿// Write your Javascript code.
+
+function showNavDrop(ev) {
+    //shows the drop down description for each link
+    var target = $(ev.target);
+    var elemText = target.text;
+    var dropPara = document.getElementById("navdropdown")
+    //dropPara.style.display = 'block';
+    switch (elemText) {
+        default:
+            dropPara.textContent =  "Default";
+        case "Bio":
+            dropPara.textContent = "Bio Show";
+            break;
+        case "":
+            break;
+    }
+}
+
+function hideNavDrop(ev) {
+    var dropPara = document.getElementById("navdropdown")
+    //dropPara.style.display = 'none';
+}
